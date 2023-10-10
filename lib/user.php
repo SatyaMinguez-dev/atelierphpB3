ed0cf14820d4b7e210ae9d038f5c3e91c46af653
SatyaMgz <satyarobben88@gmail.com>
2023-10-10 23:09:17 +0200

    troisieme commit changement dans user.php

--- lib/user.php
+++ lib/user.php
@@ -9,7 +9,7 @@ function addUser(PDO $pdo, string $first_name, string $last_name, string $email,
     $query = $pdo->prepare("INSERT INTO users(first_name,last_name,email,password) VALUES (:first_name,:last_name,:email,:password)");
     //requete d'insertion et prepare() pour des questions de sécurité
     $query -> bindValue(':first_name', $first_name, PDO::PARAM_STR);
-    $query -> bindValue(':last_name', $last_name, PDO::PARAM_STR);
+    $query -> bindValue(':last_name', $last_name, PDO::PARAM_STR); //changement de FETCH_ASSOC en PARAM_STR car last_name est un string
     $query -> bindValue(':email', $email, PDO::PARAM_STR);
     $query -> bindValue(':password', password_hash($password, PASSWORD_DEFAULT), PDO::PARAM_STR);
 
